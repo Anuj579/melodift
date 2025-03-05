208a1bcbca2f3c31143922f3a547762ed0ed76a1
Anuj Chaudhary <alwaysmile9813@gmail.com>
2025-03-05 23:34:20 +0530

    `updated`

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -143,11 +143,11 @@ export default function Home() {
                 const blob = await response.blob()
                 const url = URL.createObjectURL(blob)
                 setDownloadUrl(url)
-                toast.success("Song converted successfully! 🎵")
+                toast.success("Song converted successfully!")
             } else {
                 toast.error("Failed to process file. Please try again!")
                 setProgress(0)
-                // throw new Error("Failed to process file")
+
             }
 
         } catch (error) {
