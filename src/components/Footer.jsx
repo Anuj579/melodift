006bf0cffdecc2120d182b5515596276094d2b13
Anuj Chaudhary <alwaysmile9813@gmail.com>
2025-03-09 14:10:49 +0530

    `added linkedin link`

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -38,6 +38,16 @@ function Footer() {
                 >
                     <svg role="img" viewBox="0 0 24 24" className="h-5 w-5 text-zinc-400 hover:text-white transition-colors" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><title>YouTube</title><path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z" /></svg>
                 </motion.a>
+                <motion.a
+                    href="https://www.linkedin.com/in/anujchaudhary549/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.9 }}
+                    className="bg-zinc-800 p-2 rounded-full"
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-zinc-400 hover:text-white transition-colors" fill="currentColor" viewBox="0 0 448 512"><path d="M100.3 448H7.4V148.9h92.9zM53.8 108.1C24.1 108.1 0 83.5 0 53.8a53.8 53.8 0 0 1 107.6 0c0 29.7-24.1 54.3-53.8 54.3zM447.9 448h-92.7V302.4c0-34.7-.7-79.2-48.3-79.2-48.3 0-55.7 37.7-55.7 76.7V448h-92.8V148.9h89.1v40.8h1.3c12.4-23.5 42.7-48.3 87.9-48.3 94 0 111.3 61.9 111.3 142.3V448z" /></svg>
+                </motion.a>
             </div>
             <p className="text-zinc-500 text-xs mb-2">Melodift © {new Date().getFullYear()} • Chill vibes only</p>
             <p className="text-sm text-zinc-300">Made with 💜 by <a href="https://anujchaudhary.netlify.app" className="font-medium hover:underline underline-offset-2 text-white" target="_blank">Anuj</a></p>
