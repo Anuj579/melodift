64964804109339b6bf50e9afb64a64e8683247b9
Anuj Chaudhary <alwaysmile9813@gmail.com>
2025-03-19 11:06:52 +0530

    `updated`

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,6 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true, // Prevents ESLint from running in production
     },
-    experimental: {
-        turbo: true, // Tries to speed up the build
-    },
     output: "standalone", // Optimizes Next.js for deployment
 };
 
