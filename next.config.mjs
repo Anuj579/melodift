73be120b0ddcf528b6cae858ad82187b67bd75d6
Anuj Chaudhary <alwaysmile9813@gmail.com>
2025-03-19 11:01:16 +0530

    `Update next.config.mjs with experimental features and ESLint configuration`

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    eslint: {
+        ignoreDuringBuilds: true, // Prevents ESLint from running in production
+    },
+    experimental: {
+        turbo: true, // Tries to speed up the build
+    },
+    output: "standalone", // Optimizes Next.js for deployment
+};
 
 export default nextConfig;
